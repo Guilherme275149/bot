16679baff7067042adcbd2f46ee53ee1c22c1500
Guilherme275149 <78052265+Guilherme275149@users.noreply.github.com>
2021-01-30 19:52:10 -0300

    Add files via upload

---
+++ database/menu/wibu.js
@@ -0,0 +1,30 @@
+const wibu = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+
+         ✞ *INFO* ✞
+
+  ☛ Prefix: 「  ${prefix}  」
+  ☛ Creator: ${ownerBot}
+
+        ✞ *SOBRE* ✞
+  │
+  🔰─ ☛ ${prefix}info
+  🔰─ ☛ ${prefix}blocklist
+  🔰─ ☛ ${prefix}chatlist
+  🔰─ ☛ ${prefix}ping
+  🔰─ ☛ ${prefix}bugreport
+
+        ✞ *WIBU* ✞
+  
+  🔰─ ☛ ${prefix}neonime
+  🔰─ ☛ ${prefix}pokemon
+  🔰─ ☛ ${prefix}loli
+  🔰─ ☛ ${prefix}waifu
+  🔰─ ☛ ${prefix}randomanime
+  🔰─ ☛ ${prefix}husbu
+  🔰─ ☛ ${prefix}husbu2
+  🔰─ ☛ ${prefix}wait
+  🔰─ ☛ ${prefix}nekonime`
+}
+exports.wibu = wibu
